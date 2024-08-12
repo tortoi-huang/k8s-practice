70b9b8324b299b72f37574fb8a43f6d686177ecc
tortoi <i-40@163.com>
2024-08-12 16:10:39 +0800

    fix: 修改 3k8s-clone.ps1的错误

--- hyperv-cluster/script/host/3k8s-clone.ps1
+++ hyperv-cluster/script/host/3k8s-clone.ps1
@@ -6,7 +6,7 @@ $vm_temp = "k8s1"
 Stop-VM $vm_temp
 
 # 虚拟机k8s2
-& "$PSScriptRoot\copy-vm.ps1 -t $vm_temp -d k8s2 -m $vm_master_mem -c $vm_master_cpus"
-& "$PSScriptRoot\copy-vm.ps1 -t $vm_temp -d k8s3 -m $vm_master_mem -c $vm_master_cpus"
-& "$PSScriptRoot\copy-vm.ps1 -t $vm_temp -d k8s4 -m $vm_node_mem -c $vm_node_cups"
-& "$PSScriptRoot\copy-vm.ps1 -t $vm_temp -d k8s5 -m $vm_node_mem -c $vm_node_cups"
+& "$PSScriptRoot\copy-vm.ps1" -t $vm_temp -d k8s2 -m $vm_master_mem -c $vm_master_cpus
+& "$PSScriptRoot\copy-vm.ps1" -t $vm_temp -d k8s3 -m $vm_master_mem -c $vm_master_cpus
+& "$PSScriptRoot\copy-vm.ps1" -t $vm_temp -d k8s4 -m $vm_node_mem -c $vm_node_cups
+& "$PSScriptRoot\copy-vm.ps1" -t $vm_temp -d k8s5 -m $vm_node_mem -c $vm_node_cups
